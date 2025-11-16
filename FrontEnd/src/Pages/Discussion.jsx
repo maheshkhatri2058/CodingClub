e23536b721c42a1b68b93c8940ead5ebb4bf4ce9
Mahesh <maheshkhatri2058@gmail.com>
2025-11-16 14:12:07 +0530

    Add new files

--- FrontEnd/src/Pages/Discussion.jsx
+++ FrontEnd/src/Pages/Discussion.jsx
@@ -5,7 +5,7 @@ const Discussion = () => {
   return (
     <> 
       <div className='h-screen md:h-[560px]'>
-        <h2 className='text-2xl text-blue-800 mt-20 text-center '>we will be updating soon</h2>
+        <h2 className='text-2xl text-blue-800 mt-20 text-center '>we will be update it soon</h2>
       </div>
     </>
   )
