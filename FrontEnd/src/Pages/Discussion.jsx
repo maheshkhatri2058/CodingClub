a2c39f9bd790b12271c87cc6d421cd3413082e23
Mahesh <maheshkhatri2058@gmail.com>
2025-11-28 10:21:40 +0530

    chages

--- FrontEnd/src/Pages/Discussion.jsx
+++ FrontEnd/src/Pages/Discussion.jsx
@@ -6,9 +6,13 @@ const Discussion = () => {
   return (
     <> 
       <div className='py-20'>
-      <h1 className='text-center text-2xl font-bold'>Solved Problems:</h1>
-      <p className='  text-center px-10'>1.Given an integer array, shift all zero elements to the end while maintaining the relative order of non-zero elements<br></br></p>
-      <p className='text-center px-10'>2. Given integer array , Find the second largest element , return -1 if not found</p>
+      <div className="bg-white rounded-lg shadow-md p-6 max-w-4xl mx-auto my-8 ">
+          <h1 className='text-center text-2xl font-bold'>Solved Problems:</h1>
+      <p className='px-10'>1.Given an integer array, shift all zero elements to the end while maintaining the relative order of non-zero elements<br></br></p>
+      <p className='px-10'>2. Given integer array , Find the second largest element , return -1 if not found</p>
+
+      </div>
+  
       <ProblemCard
       title="Problem of the Day: Count Distinct Pairs With Given Sum"
 description="Given an array of integers and an integer K, find how many distinct pairs (a, b) exist such that a + b = K."
